1224d356a2b01aa1903161d47be3acfc0a65cca9
Jasleen <jasleen0859@gmail.com>
2021-10-11 16:21:09 -0500

    Added show view for individual products

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get "/products", to: "products#index", as: "products" # products_path
+  get "/products/:id", to: "products#show", as: "product" # product_path
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
