341b3467b87c0583b6ab2c0781fa1e7bc6b7e13f
Jasleen <jasleen0859@gmail.com>
2021-10-11 15:48:42 -0500

    Populated the products table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Faker::Name.unique.clear # Clears used values for Faker::Name
+Faker::UniqueGenerator.clear # Clears used values for all generators
+
+76.times do
+  Product.create(title:          Faker::Commerce.product_name,
+                 price:          Faker::Commerce.price,
+                 stock_quantity: Faker::Number.number)
+end
+
+100.times do
+  Product.create(title:          Faker::Beer.name,
+                 price:          Faker::Commerce.price,
+                 stock_quantity: Faker::Number.number)
+end
+
+100.times do
+  Product.create(title:          Faker::Coffee.blend_name,
+                 price:          Faker::Commerce.price,
+                 stock_quantity: Faker::Number.number)
+end
+
+100.times do
+  Product.create(title:          Faker::Dessert.variety,
+                 price:          Faker::Commerce.price,
+                 stock_quantity: Faker::Number.number)
+end
+
+100.times do
+  Product.create(title:          Faker::Food.dish,
+                 price:          Faker::Commerce.price,
+                 stock_quantity: Faker::Number.number)
+end
+
+100.times do
+  Product.create(title:          Faker::Music.instrument,
+                 price:          Faker::Commerce.price,
+                 stock_quantity: Faker::Number.number)
+end
+
+100.times do
+  Product.create(title:          Faker::Books::CultureSeries.book,
+                 price:          Faker::Commerce.price,
+                 stock_quantity: Faker::Number.number)
+end
