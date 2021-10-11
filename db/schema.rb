3b16fa261bacb5d6763a2eab3868925df7be6f19
Jasleen <jasleen0859@gmail.com>
2021-10-11 17:16:09 -0500

    Added a new model - Category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_11_195809) do
+ActiveRecord::Schema.define(version: 2021_10_11_221524) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
