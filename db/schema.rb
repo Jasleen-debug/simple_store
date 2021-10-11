10351e6e9ae026607e1da81ed8332efd9edcc318
Jasleen <jasleen0859@gmail.com>
2021-10-11 17:28:41 -0500

    Added foreign key reference from Product to Category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_11_221524) do
+ActiveRecord::Schema.define(version: 2021_10_11_222723) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -25,6 +25,9 @@ ActiveRecord::Schema.define(version: 2021_10_11_221524) do
     t.integer "stock_quantity"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
+  add_foreign_key "products", "categories"
 end
